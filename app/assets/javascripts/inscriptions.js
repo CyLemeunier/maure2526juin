61a9dea35e17e2b50f37e871b47079a2d4712bce
Cyrielle Lemeunier <dev@lemeunier.org>
2016-06-14 17:23:46 +0200

    JS ok sur le form new

--- app/assets/javascripts/inscriptions.js
+++ app/assets/javascripts/inscriptions.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$(document).ready(function() {
+// when a tableau is selected
+  $('select#inscription_tableau').change(function() {
+    // cache les 2 élements pour pouvoir faire simple - double et l'inverse sans refresh de la page
+    $('#joueur1').hide();
+    $('#joueur2').hide();
+    // Value of tableau selected
+    var tableau = $('select#inscription_tableau').val();
+    if(tableau == "simple homme" || tableau == "simple dame"){
+      $('#joueur1').show();
+    } else {
+      $('#joueur1').show();
+      $('#joueur2').show();
+    }
+  });
+})

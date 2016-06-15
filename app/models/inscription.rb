4273b3efc68c0cebf91fd36b73223019eb935d2b
Cyrielle Lemeunier <dev@lemeunier.org>
2016-06-15 11:32:35 +0200

    file export xls ok, suppression lien export csv

--- app/models/inscription.rb
+++ app/models/inscription.rb
@@ -3,14 +3,14 @@ SERIES = ["N2", "N3", "R4", "R5", "R6", "D7", "D8", "D9", "P"]
 PAIEMENT = ["En attente de paiement", "Payé"]
 
 class Inscription < ActiveRecord::Base
-  def self.as_csv
-    attributes = %w{nom_joueur1 prenom_joueur1 licence_joueur1 club_joueur1 tableau serie nom_joueur2 prenom_joueur2 licence_joueur2 club_joueur2 }
-
-    CSV.generate do |csv|
-      csv << attributes
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv.add_row column_names
       all.each do |inscription|
-        csv << attributes.map{ |attr| user.send(attr) }
+        values = inscription.attributes.values
+        csv.add_row values
       end
     end
   end
 end
+
